586c2da551d5a2295af48db3b8b3ef3dd381850a
prima <prima101112@gmail.com>
2014-11-01 00:25:27 +0700

    tiket pindahkan onLoadFinished

--- tryscraptiket.js
+++ tryscraptiket.js
@@ -3,61 +3,52 @@ page.viewportSize = {
     width: 1920,
     height: 1080
 };
-
-//cek finished or not
-page.onLoadFinished = function(status) {
-    console.log('Status: ' + status);
-};
-
 page.open('http://www.tiket.com/search/flight?d=SOC&a=CGK&date=2014-11-28&ret_date=&adult=1&child=0&infant=0&uid=', function() {
     page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
-
-        var fs = require('fs');
-
-        //main function scraping
-        var dom = page.evaluate(function() {
-            var outa
-            var panjang = $('.flight-rows').length;
-            var nao = [];
-            var nanana = [];
-            for (var i = 0; i < panjang; i++) {
-                var jsona = {
-                    isi: ""
-                };
-                var dat = $('.flight-rows')[i];
-                outa = dat['outerText'];
-                jsona.isi = dat['outerText'];
-                nanana[i] = jsona;
-                nao[i] = outa;
+        //cek finished or not
+        page.onLoadFinished = function(status) {
+            console.log('Status: ' + status);
+            var fs = require('fs');
+            //main function scraping
+            var dom = page.evaluate(function() {
+                var outa
+                var panjang = $('.flight-rows').length;
+                var nao = [];
+                var nanana = [];
+                for (var i = 0; i < panjang; i++) {
+                    var jsona = {
+                        isi: ""
+                    };
+                    var dat = $('.flight-rows')[i];
+                    outa = dat['outerText'];
+                    jsona.isi = dat['outerText'];
+                    nanana[i] = jsona;
+                    nao[i] = outa;
+                }
+                return nanana;
+            });
+            //function dom for duping the object
+            function dump(obj) {
+                var out = '';
+                for (var i in obj) {
+                    out += i + ": " + obj[i] + "\n";
+                }
+                console.log(out);
             }
-            return nanana;
-        });
-
-        //function dom for duping the object
-        function dump(obj) {
-            var out = '';
-            for (var i in obj) {
-                out += i + ": " + obj[i] + "\n";
-            }
-            console.log(out);
-        }
-
-        //dump dom print to console
-        dump(dom);
-
-        //create json data
-        var data = {
-            body: dom
+            //dump dom print to console
+            dump(dom);
+            //create json data
+            var data = {
+                body: dom
+            };
+            //print data to file
+            var path = 'outputtiket.json';
+            var content = JSON.stringify(data, null, 4);
+            fs.write(path, content, 'w');
+            //try to render the page
+            page.render('outputtiket.png');
+            console.log('lihat pada directori')
+            phantom.exit();
         };
-
-        //print data to file
-        var path = 'outputtiket.json';
-        var content = JSON.stringify(data, null, 4);
-        fs.write(path, content, 'w');
-
-        //try to render the page
-        page.render('outputtiket.png');
-        console.log('lihat pada directori')
-        phantom.exit();
     });
 });
